fcaa412d1590cbb9b224e2d010396eb4298f2271
Ivan Sebastian <ivansedbert@gmail.com>
2020-04-21 13:58:24 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ivan Sebastian on 21/04/20.
+//  Copyright © 2020 Vanski Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
